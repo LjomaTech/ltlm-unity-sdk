4c10c784ceab8be784155e1f6a49f91d1e2f29ef
Shacleau <ahmedabdailla@gmail.com>
2025-12-26 05:09:03 +0200

    Directing to correct API URL

--- Assets/LTLM/Scripts/Core/LTLMConstants.cs
+++ Assets/LTLM/Scripts/Core/LTLMConstants.cs
@@ -6,7 +6,7 @@ namespace LTLM.SDK.Core
         /// The fixed LTLM backend URL. 
         /// This is hardcoded to ensure it points to the correct domain when the SDK is compiled into a DLL.
         /// </summary>
-        public const string BackendUrl = "http://127.0.0.1:8000/api";
+        public const string BackendUrl = "https://ltlm.ljomatech.com:8000/api";
         
         /// <summary>
         /// Current SDK Version
